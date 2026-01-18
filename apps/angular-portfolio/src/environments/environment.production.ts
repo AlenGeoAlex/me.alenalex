cbf4e516e1d69bd8e4d9e6d4389caad381739754
AlenGeoAlex <alengeoalex@gmail.com>
2026-01-18 15:15:10 +0000

    Update API base path to use custom domain and configure Wrangler routes

--- apps/angular-portfolio/src/environments/environment.production.ts
+++ apps/angular-portfolio/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiBasePath: 'https://portfolio-backend-hono.alengeoalex123.workers.dev',
+  apiBasePath: 'https://portfolio-api.alenalex.me',
   turnstileApiKey: '0x4AAAAAACNDjiCFEeF9f2iu',
   production: true
 }
