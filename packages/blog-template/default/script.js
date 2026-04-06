5ccae18229d05db2c31fd7e27a83496a3b223550
AlenGeoAlex <alengeoalex@gmail.com>
2026-04-06 20:35:48 +0100

    Add KaTeX support for inline and block math rendering in blog template

--- packages/blog-template/default/script.js
+++ packages/blog-template/default/script.js
@@ -53,8 +53,23 @@ document.addEventListener('DOMContentLoaded', () => {
     initHighlightJs();
     buildToc();
     loadSeries();
+    renderKatex();
 });
 
+function renderKatex(){
+    renderMathInElement(document.body, {
+        // customised options
+        // • auto-render specific keys, e.g.:
+        delimiters: [
+            {left: '$$', right: '$$', display: true},
+            {left: '$', right: '$', display: false},
+            {left: '\\(', right: '\\)', display: false},
+            {left: '\\[', right: '\\]', display: true}
+        ],
+        // • rendering keys, e.g.:
+        throwOnError : false
+    });
+}
 
 function setThemeIcon() {
     const icon = document.getElementById('theme-icon');
